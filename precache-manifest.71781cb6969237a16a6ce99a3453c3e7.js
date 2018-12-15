c5a52109fc75e73f23a3678ec6aa7253b62e6560
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-12-15 12:43:54 -0500

    Updates

---
+++ precache-manifest.71781cb6969237a16a6ce99a3453c3e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "685336bc2ada27c631a9",
+    "url": "/react-gh-pages-test/static/js/runtime~main.685336bc.js"
+  },
+  {
+    "revision": "265819c4b1df58222aac",
+    "url": "/react-gh-pages-test/static/js/main.265819c4.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-gh-pages-test/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "265819c4b1df58222aac",
+    "url": "/react-gh-pages-test/static/css/main.1a5a0f54.chunk.css"
+  },
+  {
+    "revision": "05488a075c0d7ca418afcfbd830a7a2a",
+    "url": "/react-gh-pages-test/index.html"
+  }
+];
